b885341f01a49478c250849a5f839a5371e07df1
Takashi Uemura <uemura.takashi@gmail.com>
2017-12-15 15:39:03 +0900

    use single array for storing tail strings

--- sftrie_tail.hpp
+++ sftrie_tail.hpp
@@ -35,12 +35,15 @@ class sftrie_tail
 		bool match: 1;
 		bool leaf: 1;
 		integer index: bit_width<integer>() - 2;
+		integer tail;
+		integer tailend; // TODO: remove
 		symbol label;
 	};
 
 public:
-	sftrie_tail(const std::vector<text>& texts, integer min_binsearch = 16, integer min_tail = 4):
-		data(1, {false, false, 1, {}}), min_binsearch(min_binsearch), min_tail(min_tail)
+	sftrie_tail(const std::vector<text>& texts, integer min_binsearch = 16, integer min_tail = 3):
+		data(1, {false, false, 1, 0, 0, {}}), min_binsearch(min_binsearch),
+		tailstr(1, {}), min_tail(min_tail)
 	{
 		construct(texts, 0, container_size<integer>(texts), 0, 0);
 	}
@@ -81,7 +84,7 @@ private:
 	std::vector<element> data;
 	const integer min_binsearch;
 
-	std::unordered_map<integer, std::vector<symbol>> tail;
+	std::vector<symbol> tailstr;
 	const integer min_tail;
 
 	void construct(const std::vector<text>& texts, integer start, integer end, integer depth, integer current)
@@ -93,18 +96,18 @@ private:
 				return;
 			}
 		}
-		else if(start == end - 1 && container_size<integer>(texts[start]) - depth >= min_tail){
+		if(start == end - 1 && container_size<integer>(texts[start]) - depth >= min_tail){
 			data[current].leaf = true;
-			std::vector<symbol> tailstr;
+			data[current].tail = container_size<integer>(tailstr);
 			std::copy(std::begin(texts[start]) + depth, std::end(texts[start]), std::back_inserter(tailstr));
-			tail[current] = tailstr;
+			data[current].tailend = container_size<integer>(tailstr);
 			return;
 		}
 
 		// reserve siblings first
 		std::vector<integer> head{start};
 		for(integer i = start; i < end;){
-			data.push_back({false, false, 0, texts[i][depth]});
+			data.push_back({false, false, 0, 0, 0, texts[i][depth]});
 			for(symbol c = texts[i][depth]; i < end && texts[i][depth] == c; ++i);
 			head.push_back(i);
 		}
@@ -119,14 +122,12 @@ private:
 
 	bool check_tail(const text& pattern, integer i, integer current) const
 	{
-		auto p = tail.find(current);
-		if(p == std::end(tail))
+		if(data[current].tail == 0)
 			return false;
-		const auto& tailstr = p->second;
-		if(container_size<integer>(pattern) - i != container_size<integer>(tailstr))
+		else if(container_size<integer>(pattern) - i != data[current].tailend - data[current].tail)
 			return false;
 		for(integer j = i; j < container_size<integer>(pattern); ++j)
-			if(pattern[j] != tailstr[j - i])
+			if(pattern[j] != tailstr[data[current].tail + (j - i)])
 				return false;
 		return true;
 	}
