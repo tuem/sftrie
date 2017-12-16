c0806a9d1191444d03861a239a95d94c784fc941
tuem <uemura.takashi@gmail.com>
2017-12-16 12:48:09 +0900

    add simple implementation

---
+++ sftrie_simple.hpp
@@ -0,0 +1,100 @@
+/*
+sftrie
+https://github.com/tuem/sftrie
+
+Copyright 2017 Takashi Uemura
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef SFTRIE_SIMPLE_HPP
+#define SFTRIE_SIMPLE_HPP
+
+#include <vector>
+
+#include "util.hpp"
+
+template<typename text, typename integer>
+class sftrie_simple
+{
+	using symbol = typename text::value_type;
+
+	struct element
+	{
+		bool match: 1;
+		bool leaf: 1;
+		integer index: bit_width<integer>() - 2;
+		symbol label;
+	};
+
+public:
+	sftrie_simple(const std::vector<text>& texts): data(1, {false, false, 1, {}})
+	{
+		construct(texts, 0, container_size<integer>(texts), 0, 0);
+	}
+
+	bool exists(const text& pattern) const
+	{
+		integer current = 0;
+		for(integer i = 0; i < pattern.size(); ++i){
+			if(data[current].leaf)
+				return false;
+			for(integer l = data[current].index, r = data[l].index - 1; l <= r; ){
+				integer m = (l + r) / 2;
+				if(data[m].label < pattern[i]){
+					l = m + 1;
+				}
+				else if(data[m].label > pattern[i]){
+					r = m - 1;
+				}
+				else{
+					current = m;
+					goto NEXT;
+				}
+			}
+			return false;
+			NEXT:;
+		}
+		return data[current].match;
+	}
+
+private:
+	std::vector<element> data;
+
+	void construct(const std::vector<text>& texts, integer start, integer end, integer depth, integer current)
+	{
+		if(depth == container_size<integer>(texts[start])){
+			data[current].match = true;
+			if(++start == end){
+				data[current].leaf = true;
+				return;
+			}
+		}
+
+		// reserve siblings first
+		std::vector<integer> head{start};
+		for(integer i = start; i < end; head.push_back(i)){
+			data.push_back({false, false, 0, texts[i][depth]});
+			for(symbol c = texts[i][depth]; i < end && texts[i][depth] == c; ++i);
+		}
+
+		// recursively construct subtries
+		for(integer i = 0; i < container_size<integer>(head) - 1; ++i){
+			integer child = data[current].index + i;
+			data[child].index = container_size<integer>(data);
+			construct(texts, head[i], head[i + 1], depth + 1, child);
+		}
+	}
+};
+
+#endif
