2d26f36d4f621fa85cf256d6a3fcf4722d973754
tuem <uemura.takashi@gmail.com>
2017-12-18 00:29:15 +0900

    fix constructor

--- map_basic.hpp
+++ map_basic.hpp
@@ -45,7 +45,7 @@ public:
 	template<typename random_access_iterator>
 	map_basic(random_access_iterator begin, random_access_iterator end,
 			integer min_binary_search = 28):
-		data(1, {false, false, 1, {}, {}}), not_found(false, {}),
+		data(1, {false, false, 1, {}, {}}), not_found(false, data.front().value),
 		min_binary_search(min_binary_search)
 	{
 		construct(begin, end, 0, 0);
--- map_tail.hpp
+++ map_tail.hpp
@@ -48,7 +48,7 @@ public:
 	template<typename random_access_iterator>
 	map_tail(random_access_iterator begin, random_access_iterator end,
 			integer min_binary_search = 28, integer min_tail = 4):
-		data(1, {false, false, 1, 0, {}, {}}), not_found(false, {}),
+		data(1, {false, false, 1, 0, {}, {}}), not_found(false, data.front().value),
 		min_binary_search(min_binary_search), tails(1, {}), min_tail(min_tail)
 	{
 		construct(begin, end, 0, 0);
