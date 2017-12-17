091fa4850b7b9cf2601bf6aff79ce6ae1323b7cb
tuem <uemura.takashi@gmail.com>
2017-12-17 23:01:21 +0900

    fix include guard

--- map_naive.hpp
+++ map_naive.hpp
@@ -17,8 +17,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#ifndef SFTRIE_NAIVE_HPP
-#define SFTRIE_NAIVE_HPP
+#ifndef SFTRIE_MAP_NAIVE_HPP
+#define SFTRIE_MAP_NAIVE_HPP
 
 #include <vector>
 
