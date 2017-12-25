43f886949b2aa1234c00e985374d8846381b4a95
tuem <uemura.takashi@gmail.com>
2017-12-25 10:50:11 +0900

    update benchmark_map

--- eval/benchmark_map.cpp
+++ eval/benchmark_map.cpp
@@ -26,7 +26,11 @@ limitations under the License.
 #include <random>
 #include <chrono>
 
-#include <sftrie/map.hpp>
+#include <sftrie/map_naive.hpp>
+#include <sftrie/map_basic.hpp>
+#include <sftrie/map_tail.hpp>
+#include <sftrie/map_decompaction.hpp>
+#include <paramset.hpp>
 
 #include "string_util.hpp"
 #include "history.hpp"
@@ -34,8 +38,21 @@ limitations under the License.
 using object = unsigned int;
 using integer = unsigned int;
 
+template<typename text_object_pair, typename map>
+size_t evaluate(const map& dict, const std::vector<text_object_pair>& queries)
+{
+	size_t found = 0;
+	for(const auto& query: queries){
+		auto result = dict.find(query.first);
+		if(result.first && result.second == query.second)
+			++found;
+	}
+	return found;
+}
+
 template<typename text, typename object, typename integer>
-int exec(const std::string& corpus_path)
+int exec(const std::string& corpus_path, const std::string& sftrie_type,
+	int min_binary_search, int min_tail, int min_decompaction)
 {
 	History history;
 
@@ -73,54 +90,136 @@ int exec(const std::string& corpus_path)
 	std::cerr << "done." << std::endl;
 	history.record("(prepare)");
 
-	std::cerr << "constructing index...";
-	sftrie::map<text, object, integer> dict(std::begin(texts), std::end(texts));
-	std::cerr << "done." << std::endl;
-	history.record("construction", texts.size());
+	size_t space = 0;
+	size_t found_ordered = 0, found_shuffled = 0;
+	if(sftrie_type == "naive"){
+		std::cerr << "constructing index...";
+		sftrie::map_naive<text, object, integer> dict(std::begin(texts), std::end(texts));
+		history.record("construction", texts.size());
+		std::cerr << "done." << std::endl;
+		space = dict.space();
 
-	std::cerr << "searching (ordered)...";
-	size_t found = 0;
-	for(const auto& query: queries)
-		if(dict.find(query.first).first)
-			++found;
-	std::cerr << "done." << std::endl;
-	history.record("search (ordered)", queries.size());
+		std::cerr << "searching (ordered)...";
+		found_ordered = evaluate(dict, queries);
+		history.record("search (ordered)", queries.size());
+		std::cerr << "done." << std::endl;
 
-	std::cerr << "searching (shuffled)...";
-	size_t found_shuffled = 0;
-	for(const auto& query: shuffled_queries)
-		if(dict.find(query.first).first)
-			++found_shuffled;
-	std::cerr << "done." << std::endl;
-	history.record("search (shuffled)", shuffled_queries.size());
+		std::cerr << "searching (shuffled)...";
+		found_shuffled = evaluate(dict, shuffled_queries);
+		history.record("search (shuffled)", shuffled_queries.size());
+		std::cerr << "done." << std::endl;
+	}
+	else if(sftrie_type == "basic"){
+		std::cerr << "constructing index...";
+		sftrie::map_basic<text, object, integer> dict(std::begin(texts), std::end(texts),
+			min_binary_search);
+		history.record("construction", texts.size());
+		std::cerr << "done." << std::endl;
+		space = dict.space();
+
+		std::cerr << "searching (ordered)...";
+		found_ordered = evaluate(dict, queries);
+		history.record("search (ordered)", queries.size());
+		std::cerr << "done." << std::endl;
+
+		std::cerr << "searching (shuffled)...";
+		found_shuffled = evaluate(dict, shuffled_queries);
+		history.record("search (shuffled)", shuffled_queries.size());
+		std::cerr << "done." << std::endl;
+	}
+	else if(sftrie_type == "tail"){
+		std::cerr << "constructing index...";
+		sftrie::map_tail<text, object, integer> dict(std::begin(texts), std::end(texts),
+			min_binary_search, min_tail);
+		history.record("construction", texts.size());
+		std::cerr << "done." << std::endl;
+		space = dict.space();
+
+		std::cerr << "searching (ordered)...";
+		found_ordered = evaluate(dict, queries);
+		history.record("search (ordered)", queries.size());
+		std::cerr << "done." << std::endl;
+
+		std::cerr << "searching (shuffled)...";
+		found_shuffled = evaluate(dict, shuffled_queries);
+		history.record("search (shuffled)", shuffled_queries.size());
+		std::cerr << "done." << std::endl;
+	}
+	else if(sftrie_type == "decompaction"){
+		std::cerr << "constructing index...";
+		sftrie::map_decompaction<text, object, integer> dict(std::begin(texts), std::end(texts),
+			min_binary_search, min_tail, min_decompaction);
+		history.record("construction", texts.size());
+		std::cerr << "done." << std::endl;
+		space = dict.space();
+
+		std::cerr << "searching (ordered)...";
+		found_ordered = evaluate(dict, queries);
+		history.record("search (ordered)", queries.size());
+		std::cerr << "done." << std::endl;
+
+		std::cerr << "searching (shuffled)...";
+		found_shuffled = evaluate(dict, shuffled_queries);
+		history.record("search (shuffled)", shuffled_queries.size());
+		std::cerr << "done." << std::endl;
+	}
 
 	std::cout << std::endl;
 	std::cout << "size:" << std::endl;
-	std::cout << std::setw(16) << "# of texts: " << std::setw(16) << dict.size() << std::endl;
+	std::cout << std::setw(16) << "# of texts: " << std::setw(16) << texts.size() << std::endl;
 	std::cout << std::setw(16) << "total length: " << std::setw(16) << total_length << std::endl;
-	std::cout << std::setw(16) << "index size: " << std::setw(16) << dict.space() << std::endl;
+	std::cout << std::setw(16) << "index size: " << std::setw(16) << space << std::endl;
 	std::cout << std::endl;
 	std::cout << "time:" << std::endl;
 	history.dump(std::cout, true, true);
 
-	return found == queries.size() && found_shuffled == shuffled_queries.size();
+	return (found_ordered == queries.size() && found_shuffled == shuffled_queries.size()) ? 0 : 1;
 }
 
 int main(int argc, char* argv[])
 {
-	if(argc < 2){
-		std::cerr << "usage: " << argv[0] << " corpus" << std::endl;
-		return 0;
-	}
+	paramset::definitions defs = {
+		{"symbol_type", "char", "map-symbol-type", 's', "symbol type"},
+		{"sftrie_type", "naive", "map-sftrie-type", 't', "sftrie type"},
+		{"min_binary_search", 42, {"map", "min_binary_search"}, "map-min-binary-search", 0, "minumum number of children for binary search"},
+		{"min_tail", 1, {"map", "min_tail"}, "map-min-tail", 0, "minumum length to copress tail strings"},
+		{"min_decompaction", 0, {"map", "min_decompaction"}, "map-min-decompaction", 0, "minumum number of children to enable decompaction"},
+		{"conf_path", "", "config", 'c', "config file path"}
+	};
+	paramset::manager pm(defs);
 
-	std::string corpus_path = argv[1];
-	std::string type = argc > 2 ? argv[2] : "s";
-	if(type == "u16")
-		return exec<std::u16string, object, integer>(corpus_path);
-	else if(type == "u32")
-		return exec<std::u32string, object, integer>(corpus_path);
-	else if(type == "w")
-		return exec<std::wstring, object, integer>(corpus_path);
-	else
-		return exec<std::string, object, integer>(corpus_path);
+	try{
+		pm.load(argc, argv, "config");
+
+		if(!pm.rest.empty())
+			pm["corpus_path"] = pm.rest.front().as<std::string>();
+
+		std::string corpus_path = pm["corpus_path"];
+		std::string symbol_type = pm["symbol_type"];
+		std::string sftrie_type = pm["sftrie_type"];
+		int min_binary_search = pm["min_binary_search"];
+		int min_tail = pm["min_tail"];
+		int min_decompaction = pm["min_decompaction"];
+
+		std::cerr << "Configuration" << std::endl;
+		std::cerr << std::setw(12) << std::left << "  symbol_type: " << symbol_type << std::endl;
+		std::cerr << std::setw(12) << std::left << "  sftrie_type: " << sftrie_type << std::endl;
+		std::cerr << std::setw(12) << std::left << "  min_binary_search: " << min_binary_search << std::endl;
+		std::cerr << std::setw(12) << std::left << "  min_tail: " << min_tail << std::endl;
+		std::cerr << std::setw(12) << std::left << "  min_decompaction: " << min_decompaction << std::endl;
+		std::cerr << std::endl;
+
+		if(symbol_type == "char")
+			return exec<std::string, object, integer>(corpus_path, sftrie_type, min_binary_search, min_tail, min_decompaction);
+		else if(symbol_type == "wchar_t")
+			return exec<std::wstring, object, integer>(corpus_path, sftrie_type, min_binary_search, min_tail, min_decompaction);
+		else if(symbol_type == "char16_t")
+			return exec<std::u16string, object, integer>(corpus_path, sftrie_type, min_binary_search, min_tail, min_decompaction);
+		else if(symbol_type == "char32_t")
+			return exec<std::u32string, object, integer>(corpus_path, sftrie_type, min_binary_search, min_tail, min_decompaction);
+	}
+	catch(const std::exception& e){
+		std::cerr << "error: " << e.what() << std::endl;
+		exit(1);
+	}
 }
