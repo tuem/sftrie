648cea2b21b473f40ffca8ba8e7b9ced3ea47859
tuem <uemura.takashi@gmail.com>
2017-12-27 13:06:27 +0900

    make pair of const references correctly

--- map_tail.hpp
+++ map_tail.hpp
@@ -248,7 +248,7 @@ struct map_tail<text, object, integer>::common_prefix_iterator
 
 	const std::pair<const text&, const object&> operator*() const
 	{
-		return std::make_pair(result, data[path.back()].value);
+		return std::pair<const text&, const object&>(result, data[path.back()].value);
 	}
 
 	common_prefix_iterator& operator++()
