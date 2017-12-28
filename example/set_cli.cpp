93fec51c4f57aed11f105e256eb5d02b1459806a
tuem <uemura.takashi@gmail.com>
2017-12-28 13:45:32 +0900

    use default sftrie::set

--- example/set_cli.cpp
+++ example/set_cli.cpp
@@ -21,7 +21,7 @@ limitations under the License.
 #include <fstream>
 #include <string>
 
-#define SFTRIE_SET_USE_NAIVE
+//#define SFTRIE_SET_USE_NAIVE
 //#define SFTRIE_SET_USE_BASIC
 //#define SFTRIE_SET_USE_TAIL
 //#define SFTRIE_SET_USE_DECOMPACTION
