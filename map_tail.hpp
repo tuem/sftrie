5fbfbf8beec75eddb7968a32fcf183c949d4c0f7
tuem <uemura.takashi@gmail.com>
2018-12-09 10:20:03 +0000

    implement common-prefix search in map_tail

--- map_tail.hpp
+++ map_tail.hpp
@@ -33,8 +33,9 @@ class map_tail
 	using result = std::pair<bool, const object&>;
 
 	struct element;
-	struct traversal_searcher;
+	struct common_searcher;
 	struct traversal_iterator;
+	struct prefix_iterator;
 
 public:
 	template<typename random_access_iterator>
@@ -43,7 +44,7 @@ public:
 std::size_t size() const;
 	std::size_t space() const;
 	result find(const text& pattern) const;
-	traversal_searcher searcher() const;
+	common_searcher searcher() const;
 
 private:
 	const std::size_t num_texts;
@@ -126,10 +127,10 @@ map_tail<text, object, integer>::find(const text& pattern) const
 }
 
 template<typename text, typename object, typename integer>
-typename map_tail<text, object, integer>::traversal_searcher
+typename map_tail<text, object, integer>::common_searcher
 map_tail<text, object, integer>::searcher() const
 {
-	return traversal_searcher(*this);
+	return common_searcher(*this);
 }
 
 template<typename text, typename object, typename integer>
@@ -192,7 +193,7 @@ bool map_tail<text, object, integer>::check_tail_prefix(const text& pattern, int
 }
 
 template<typename text, typename object, typename integer>
-struct map_tail<text, object, integer>::traversal_searcher
+struct map_tail<text, object, integer>::common_searcher
 {
 	const map_tail<text, object, integer>& index;
 
@@ -201,7 +202,7 @@ struct map_tail<text, object, integer>::traversal_searcher
 	std::vector<integer> path_end;
 	text result_end;
 
-	traversal_searcher(const map_tail<text, object, integer>& index): index(index){}
+	common_searcher(const map_tail<text, object, integer>& index): index(index){}
 
 	traversal_iterator traverse(const text& pattern)
 	{
@@ -225,6 +226,12 @@ struct map_tail<text, object, integer>::traversal_searcher
 				return traversal_iterator(index, path_end, result_end);
 		}
 		return traversal_iterator(index, path, result, path_end, result_end, current, pattern);
+	}
+
+	prefix_iterator prefix(const text& pattern)
+	{
+		result.clear();
+		return prefix_iterator(*this, pattern, 0, 0);
 	}
 };
 
@@ -311,6 +318,83 @@ struct map_tail<text, object, integer>::traversal_iterator
 			}
 		}while(!path.empty() && !index.data[path.back()].match &&
 			!(path.size() > 1 && path.back() == path[path.size() - 2]));
+		return *this;
+	}
+};
+
+template<typename text, typename object, typename integer>
+struct map_tail<text, object, integer>::prefix_iterator
+{
+	common_searcher& searcher;
+	const text& pattern;
+	integer current;
+	integer depth;
+
+	prefix_iterator(common_searcher& searcher, const text& pattern, integer current, integer depth):
+		searcher(searcher), pattern(pattern), current(current), depth(depth)
+	{
+		if(current == 0 && !searcher.index.data[current].match){
+			if(pattern.empty())
+				this->current = searcher.index.data.size() - 1;
+			else
+				++*this;
+		}
+	}
+
+	prefix_iterator& begin()
+	{
+		return *this;
+	}
+
+	prefix_iterator end() const
+	{
+		return prefix_iterator(searcher, pattern, searcher.index.data.size() - 1, pattern.size());
+	}
+
+	bool operator!=(const prefix_iterator& i) const
+	{
+		return this->current != i.current;
+	}
+
+	const std::pair<const text&, const object&> operator*() const
+	{
+		return std::pair<const text&, const object&>(searcher.result, searcher.index.data[current].value);
+	}
+
+	prefix_iterator& operator++()
+	{
+		if(!searcher.index.data[current].leaf){
+			while(depth < pattern.size()){
+				if(searcher.index.data[current].leaf){
+					if(
+						container_size<integer>(pattern) - depth >= searcher.index.data[current + 1].tail - searcher.index.data[current].tail &&
+						std::equal(
+							std::begin(pattern) + depth,
+							std::begin(pattern) + depth + (searcher.index.data[current + 1].tail - searcher.index.data[current].tail),
+							std::begin(searcher.index.tails) + searcher.index.data[current].tail
+						)
+					)
+						std::copy(std::begin(searcher.index.tails) + searcher.index.data[current].tail,
+							std::begin(searcher.index.tails) + searcher.index.data[current + 1].tail, std::back_inserter(searcher.result));
+					else
+						current = searcher.index.data.size() - 1;
+					return *this;
+				}
+				current = searcher.index.data[current].next;
+				integer end = searcher.index.data[current].next;
+				for(integer w = end - current, m; w > searcher.index.min_binary_search; w = m){
+					m = w >> 1;
+					current += searcher.index.data[current + m].label < pattern[depth] ? w - m : 0;
+				}
+				for(; current < end && searcher.index.data[current].label < pattern[depth]; ++current);
+				if(!(current < end && searcher.index.data[current].label == pattern[depth]))
+					break;
+				searcher.result.push_back(pattern[depth++]);
+				if(searcher.index.data[current].match)
+					return *this;
+			}
+		}
+		current = searcher.index.data.size() - 1;
 		return *this;
 	}
 };
