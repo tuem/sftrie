91215524944879c21d9c01e8b497b516e1d13eba
tuem <uemura.takashi@gmail.com>
2023-09-01 16:45:00 +0900

    delete unnecessary code

--- include/sftrie/set_compact.hpp
+++ include/sftrie/set_compact.hpp
@@ -281,7 +281,7 @@ integer set_compact<text, integer>::load(input_stream& is)
 	data.resize(header.node_count);
 	is.read(reinterpret_cast<char*>(data.data()), static_cast<std::streamsize>(sizeof(node) * header.node_count));
 
-	labels.resize(header.tail_length);
+	labels.resize(header.label_count);
 	is.read(reinterpret_cast<char*>(labels.data()), static_cast<std::streamsize>(sizeof(symbol) * header.label_count));
 
 	return std::count_if(data.begin(), data.end(), [](const auto& n){
@@ -640,10 +640,7 @@ struct set_compact<text, integer>::prefix_iterator
 
 	prefix_iterator& operator++()
 	{
-		for(; !searcher.trie.data[current].leaf && depth < pattern.size(); ){
-			if(searcher.trie.data[current].leaf)
-				break;
-
+		while(!searcher.trie.data[current].leaf && depth < pattern.size()){
 			// find child
 			current = searcher.trie.data[current].next;
 			integer end = searcher.trie.data[current].next;
