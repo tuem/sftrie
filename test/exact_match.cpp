622645dfa623acd4c553621936fa311025b3d73c
tuem <uemura.takashi@gmail.com>
2023-09-08 14:27:04 +0900

    merge exact matching tests

--- test/exact_match.cpp
+++ test/exact_match.cpp
@@ -24,11 +24,16 @@ limitations under the License.
 #include <Catch2/catch.hpp>
 
 #include <sftrie/set.hpp>
+#include <sftrie/map.hpp>
 
 #include "string_util.hpp"
 
 
 using integer = std::uint32_t;
+using item = integer;
+
+
+// utility for set
 
 template<typename set>
 void test_set_exact_match(
@@ -100,7 +105,109 @@ void test_set_exact_match_all(
 	}
 }
 
-TEST_CASE("set/empty set", "[set]"){
+
+// utility for map
+
+template<typename text>
+std::vector<std::pair<text, item>> assign_ids(const std::vector<text>& texts, item start = 1)
+{
+	std::vector<std::pair<text, item>> results;
+	auto i = start;
+	for(const auto& t: texts)
+		results.push_back({t, ++i});
+	return results;
+}
+
+template<typename map>
+void test_map_exact_match(
+	const std::vector<std::pair<typename map::text_type, typename map::value_type>>& texts,
+	const std::vector<typename map::text_type>& patterns_not_in_texts,
+	typename map::integer_type expected_size = 0
+)
+{
+	map index(texts.begin(), texts.end());
+	if(expected_size > 0){
+		SECTION("trie size"){
+			CHECK(index.trie_size() == expected_size); // root and sentinel
+		}
+	}
+
+	SECTION("search patterns in texts"){
+		for(const auto& [pattern, expected_value]: texts){
+			CHECK(index.exists(pattern));
+			auto v0 = index[pattern];
+			CHECK(v0 == expected_value);
+			auto v1 = index.find(pattern).value();
+			CHECK(v1 == expected_value);
+		}
+	}
+
+	SECTION("search patterns not in texts"){
+		for(const auto& pattern: patterns_not_in_texts){
+			CHECK(!index.exists(pattern));
+			auto n = index.find(pattern);
+			if(n != index.root()) // root is always valid
+				CHECK(!n.match());
+		}
+	}
+}
+
+template<typename text>
+void test_map_exact_match_all_classes(
+	const std::vector<std::pair<text, item>>& texts,
+	const std::vector<text>& patterns_not_in_texts,
+	size_t expected_size_original = 0,
+	size_t expected_size_compact = 0
+)
+{
+	SECTION("map_original"){
+		test_map_exact_match<sftrie::map_original<text, item, integer>>(texts,
+			patterns_not_in_texts, expected_size_original);
+	}
+	SECTION("map_compact"){
+		test_map_exact_match<sftrie::map_compact<text, item, integer>>(texts,
+			patterns_not_in_texts, expected_size_compact);
+	}
+}
+
+void test_map_exact_match_all(
+	const std::vector<std::string>& texts,
+	const std::vector<std::string>& patterns_not_in_texts,
+	std::map<std::string, size_t>& expected_sizes
+)
+{
+	auto text_ids = assign_ids(texts);
+
+	SECTION("char"){
+		test_map_exact_match_all_classes(text_ids, patterns_not_in_texts, expected_sizes["original/char"], expected_sizes["compact/char"]);
+	}
+
+	SECTION("char16_t"){
+		auto text_ids_u16 = cast_strings<std::u16string>(text_ids);
+		auto patterns_not_in_texts_u16 = cast_strings<std::u16string>(patterns_not_in_texts);
+		test_map_exact_match_all_classes(text_ids_u16, patterns_not_in_texts_u16, expected_sizes["original/char16_t"], expected_sizes["compact/char16_t"]);
+	}
+
+	SECTION("char32_t"){
+		auto text_ids_u32 = cast_strings<std::u32string>(text_ids);
+		auto patterns_not_in_texts_u32 = cast_strings<std::u32string>(patterns_not_in_texts);
+		test_map_exact_match_all_classes(text_ids_u32, patterns_not_in_texts_u32, expected_sizes["original/char32_t"], expected_sizes["compact/char32_t"]);
+	}
+}
+
+
+void test_exact_match_all(
+	const std::vector<std::string>& texts,
+	const std::vector<std::string>& patterns_not_in_texts,
+	std::map<std::string, size_t>& expected_sizes
+)
+{
+	test_set_exact_match_all(texts, patterns_not_in_texts, expected_sizes);
+	test_map_exact_match_all(texts, patterns_not_in_texts, expected_sizes);
+}
+
+
+TEST_CASE("exact match/empty set", "[exact match]"){
 	using text = std::string;
 
 	const std::vector<text> texts = {
@@ -118,7 +225,7 @@ TEST_CASE("set/empty set", "[set]"){
 		{"compact/char32_t", 2},
 	};
 
-	test_set_exact_match_all(texts, patterns_not_in_texts, expected_sizes);
+	test_exact_match_all(texts, patterns_not_in_texts, expected_sizes);
 }
 
 TEST_CASE("set/set of an empty string", "[set]"){
@@ -139,7 +246,7 @@ TEST_CASE("set/set of an empty string", "[set]"){
 		{"compact/char32_t", 2},
 	};
 
-	test_set_exact_match_all(texts, patterns_not_in_texts, expected_sizes);
+	test_exact_match_all(texts, patterns_not_in_texts, expected_sizes);
 }
 
 TEST_CASE("set/set of a string with a single symbol", "[set]"){
@@ -161,7 +268,7 @@ TEST_CASE("set/set of a string with a single symbol", "[set]"){
 		{"compact/char32_t", 3},
 	};
 
-	test_set_exact_match_all(texts, patterns_not_in_texts, expected_sizes);
+	test_exact_match_all(texts, patterns_not_in_texts, expected_sizes);
 }
 
 TEST_CASE("set/set of a string", "[set]"){
@@ -188,5 +295,39 @@ TEST_CASE("set/set of a string", "[set]"){
 		{"compact/char32_t", 3},
 	};
 
-	test_set_exact_match_all(texts, patterns_not_in_texts, expected_sizes);
+	test_exact_match_all(texts, patterns_not_in_texts, expected_sizes);
+}
+
+TEST_CASE("set/few texts", "[set]"){
+	using text = std::string;
+
+	const std::vector<text> texts = {
+		"AM",
+		"AMD",
+		"CAD",
+		"CAM",
+		"CM",
+		"DM",
+	};
+	const std::vector<text> patterns_not_in_texts = {
+		"",
+		" ",
+		"A",
+		"B",
+		"AD",
+		"CA",
+		"CD",
+		"CAME",
+		"E",
+	};
+	std::map<std::string, size_t> expected_sizes = {
+		{"original/char", 12},
+		{"compact/char", 10},
+		{"original/char16_t", 12},
+		{"compact/char16_t", 10},
+		{"original/char32_t", 12},
+		{"compact/char32_t", 10},
+	};
+
+	test_exact_match_all(texts, patterns_not_in_texts, expected_sizes);
 }
